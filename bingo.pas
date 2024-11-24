fa0e51c6df6e1f863a007e3a2034b2e46f9be1f3
Alejandro Malave <Alejandromfer15@gmail.com>
2024-11-24 17:07:19 -0400

    Definición de funciones auxiliares que ayudarán a desarrollar el juego de bingo

--- bingo.pas
+++ bingo.pas
@@ -21,6 +21,47 @@ var
   BolasExtraidas: array[1..NUM_BOLAS] of integer;
   Ganador: integer;
 
+function LeerNumero(mensaje: string): integer;
+var
+  numero: integer;
+  error: integer;
+  entrada: string;
+begin
+  repeat
+    write(mensaje);
+    readln(entrada);
+    val(entrada, numero, error);
+    
+    if error <> 0 then
+      writeln('Error: Debe ingresar un número válido');
+      
+  until error = 0;
+
+  LeerNumero := numero;
+end;
+
+function ExisteEnArreglo(var arreglo: array of integer; valor: integer): boolean;
+var
+  existe: boolean;
+  i: integer;
+begin
+    existe := False;
+
+    for i := Low(arreglo) to High(arreglo) do
+        if (arreglo[i] = valor) then
+        begin
+            existe := True;
+            Break;
+        end;
+    
+    ExisteEnArreglo := existe;
+end;
+
+function GenerarNumeroAleatorio(min, max: integer): integer;
+begin
+  GenerarNumeroAleatorio := Random(max - min + 1) + min;
+end;
+
 // ejecución del programa
 begin
   NUM_JUGADORES := LeerNumero('Ingrese el número de jugadores: ');
